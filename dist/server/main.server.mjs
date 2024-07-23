06fc95f402284c0acc525aa8ca9a61b21ff8d8b1
surya-sks <suryaskandasamy@gmail.com>
2024-07-23 14:10:46 +0530

    href and dist file

--- dist/server/main.server.mjs
+++ dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-K3HS4YE4.mjs";import"./chunk-HE5PF6OI.mjs";import"./chunk-W3JOJUPU.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-AVRLIJEY.mjs";import"./chunk-HE5PF6OI.mjs";import"./chunk-W3JOJUPU.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
